257e10418af6c3a63652d1f7b0e0504c9d0c57bd
Robert Roach <rjayroach@gmail.com>
2021-11-25 08:39:12 +0800

    rename service

--- backend/app/models/kafka/service.rb
+++ backend/app/models/kafka/service.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class Service::Kafka < Service
+class Kafka::Service < Service
   store :config, accessors: %i[security_protocol sasl_mechanism username password configuration_overrides], coder: YAML
   store :config, accessors: %i[bootstrap_servers ui], coder: YAML
 
